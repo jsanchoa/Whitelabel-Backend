8e8b51984e5625d62e2110a859d5ea3c1575fb9e
Jose Sancho <jsancho60367@ufide.ac.cr>
2025-08-19 15:11:29 -0600

    Minor changes to index

--- src/database/DatabaseConnection.js
+++ src/database/DatabaseConnection.js
@@ -3,15 +3,15 @@ import { Sequelize } from "sequelize";
 const database = new Sequelize
 ({
   dialect: 'oracle',
-  //username: 'C##WHITELABEL',
-   username: 'WHITELABEL',
+  username: 'C##WHITELABEL',
+  //  username: 'WHITELABEL',
   password: 'proyecto',
   host: 'localhost',
-  //port: 1521,
-   port: 11521,
+  port: 1521,
+  //  port: 11521,
   dialectOptions: {
-    //connectString: 'localhost:1521/XE'
-     connectString: 'localhost:11521/XEPDB1'
+    connectString: 'localhost:1521/XE'
+    //  connectString: 'localhost:11521/XEPDB1'
   },
   quoteIdentifiers: false
 });
