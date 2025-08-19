da4560aeba20088a78f9403011bd90ea813a6cfb
Noelia Segura <156636908+NoeSegura11@users.noreply.github.com>
2025-08-19 16:52:11 -0600

    Providers Fix Backend

--- src/controllers/ProviderController.js
+++ src/controllers/ProviderController.js
@@ -1,7 +1,16 @@
-import { getProviders, hideProviders } from "../services/ProviderServices.js";
+import { getProviderInfo, getProviders, hideProviders, modifyProvider, newProvider } from "../services/ProviderServices.js";
 
 export const ListProviders = async(req, res) => {
     const data = await getProviders();
+
+    res.send(data);
+}
+
+export const infoProvider = async(req, res) => {
+
+    const id = req.params.id;
+
+    const data = await getProviderInfo(id);
 
     res.send(data);
 }
@@ -11,13 +20,29 @@ export const addProvider = async(req, res) => {
 
     try {
         //Enviar la informacion al service
-        const addProvider = await newProviders(data);
+        const addProvider = await newProvider(data);
 
 
         res.send({ success: true, message: "A new Provider has been created sucessfully."})
 
     } catch(error) {
         res.send({ success: false, message: "A new Provider can not be created."})
+    }
+
+}
+
+export const editProvider = async(req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        //Mdoificar la informacion al service
+        await modifyProvider(id, data);
+
+        res.send({ success: true, message: "A new Provider has been modified sucessfully."})
+
+    } catch(error) {
+        res.send({ success: false, message: "A new Provider can not be modified."})
     }
 
 }
